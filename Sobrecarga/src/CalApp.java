05bad7bdf23ea923de7a1d4e49fa615c913d685e
tute20 <tute2493@gmail.com>
2017-06-05 21:06:09 -0300

    asd
    asd

--- Sobrecarga/src/CalApp.java
+++ Sobrecarga/src/CalApp.java
@@ -4,5 +4,17 @@ public static void main(String[] args) {
 	Calculadora calc = new Calculadora();
 	System.out.println("Suma");
 	System.out.println(calc.suma("20.3","33.44","2.0","77.88","41.32"));
+	
+	System.out.println("Suma");
+	System.out.println(calc.suma("12","33"));
+	
+	System.out.println("Suma");
+	System.out.println(calc.suma("3","5","2"));
+	
+	System.out.println("Suma");
+	System.out.println(calc.suma("3.2","2.3"));
+	
+	System.out.println("Suma");
+	System.out.println(calc.suma("10.5","33.4","2.1"));
 }
 }
